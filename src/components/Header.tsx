5c1c3128292518ae9d7d5b87a43c2543067069d7
yukicoder0509 <bendoris0509@gmail.com>
2025-08-29 13:37:23 +0800

    feat: add Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header(){
+    return (
+        <div className="w-full py-2.5 px-5 shadow-md">
+            <h1 className="text-xl font-bold">SDC Task</h1>
+        </div>
+    )
+}
